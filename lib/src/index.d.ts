b210adddfc14906bd24fd7d6ea9b1e6f05b79c40
whoastonic <kingstonicthe1@gmail.com>
2021-04-11 05:23:19 -0400

    forgot to build...

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -1,4 +1,4 @@
 export { Config, loadConfig, } from './config';
 export * from './YGOProAPI';
 export { logger } from './logger';
-export { CommonModule } from './rest/server';
+export * as RESTModules from './rest';
